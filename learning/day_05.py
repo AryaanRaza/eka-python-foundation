099ff64dab9594f9905b96cbb16dca9673d3c849
Aryaan Raza <aryaan.raza.dev@gmail.com>
2026-08-18 22:35:54 +0530

    day 5: add knowledge validation

---
+++ learning/day_05.py
@@ -0,0 +1,10 @@
+category = input("Enter knowledge category: ")
+
+if category == "HR":
+    print("This knowledge belongs to HR")
+
+elif category == "IT":
+    print("This knowledge belongs to Information Technology")
+
+else:
+    print("This is another type of knowledge")
--- main_eka/main.py
+++ main_eka/main.py
@@ -4,6 +4,19 @@ version = "0.3"
 knowledge = []
 
 def create_knowledge(title , content , category , source):
+
+    if title == "":
+        print("Title cannot be empty.")
+        return None
+
+    if content == "":
+        print("Error: Content cannot be empty.")
+        return None
+
+    if category == "":
+        print("Error: Category cannot be empty.")
+        return None
+
     knowledge_item = {
         "title": title,
         "content": content,
@@ -18,16 +31,24 @@ print(project_name)
 print(f"Version: {version}")
 print("=" * 40)
 
-for i in range(3):
+for i in range(2):
     print(f"\nKnowledge #{i + 1}")
 
     title = input("Enter knowledge title: ")
     content = input("Enter knowledge: ")
     category = input("Enter knowledge category: ")
+    if category == "HR":
+        print("Category recognized: Human Resources")
+
+    elif category == "IT":
+        print("Category recognized: Information Technology")
+    else:
+        print("Category: General")
     source = input("Enter knowledge source: ")
 
     knowledge_item = create_knowledge(title , content , category , source)
-    knowledge.append(knowledge_item)
+    if(knowledge_item is not None):
+        knowledge.append(knowledge_item)
 
 print("\n=== EKA Knowledge Base ===")
 
